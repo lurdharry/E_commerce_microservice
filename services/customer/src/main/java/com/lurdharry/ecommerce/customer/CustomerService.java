1c0a2443e87e6925e97a635de2de216305640ad0
lurdharry <azeezhammed57@gmail.com>
2025-02-18 15:22:27 +0100

    add update customer handler

--- services/customer/src/main/java/com/lurdharry/ecommerce/customer/CustomerService.java
+++ services/customer/src/main/java/com/lurdharry/ecommerce/customer/CustomerService.java
@@ -1,11 +1,12 @@
 package com.lurdharry.ecommerce.customer;
 
-import com.lurdharry.ecommerce.customer.models.Customer;
 import com.lurdharry.ecommerce.exception.CustomerNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
@@ -25,10 +26,36 @@ public class CustomerService {
         var customer =  customerRepository.findById(request.id())
                 .orElseThrow(()-> new CustomerNotFoundException(
                         format(
-                                "Cannot update customer:: no customer found with the ID:: %S", request.id()
+                                "Cannot update customer:: no customer found with the ID:: %s", request.id()
                         )
                 ));
         customerMapper.mergeCustomer(customer, request);
         customerRepository.save(customer);
+    }
+
+    public List<CustomerResponse> findAllCustomer() {
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::fromCustomer)
+                .collect(Collectors.toList());
+    }
+
+    public Boolean existsById(String customerId) {
+
+        return customerRepository.findById(customerId).isPresent();
+    }
+
+    public CustomerResponse findById(String customerId) {
+
+        return customerRepository.findById(customerId)
+                .map(customerMapper::fromCustomer)
+                .orElseThrow(()-> new CustomerNotFoundException(
+                        format("No customer found with the provided ID:: %s", customerId)
+                ));
+    }
+
+    public void deleteCustomer(String customerId) {
+
+        customerRepository.deleteById(customerId);
     }
 }
