7aecd821e75e6239e7d431668ff537e9596198f1
lurdharry <azeezhammed57@gmail.com>
2025-02-19 15:15:41 +0100

    add product mapper functions

---
+++ services/product/src/main/java/com/lurdharry/ecommerce/product/ProductMapper.java
@@ -0,0 +1,50 @@
+package com.lurdharry.ecommerce.product;
+
+
+import com.lurdharry.ecommerce.category.Category;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductMapper {
+    
+    public Product toProduct(@Valid ProductRequest request) {
+
+        return Product.builder()
+                .id(request.id())
+                .name(request.name())
+                .description(request.description())
+                .price(request.price())
+                .availableQuantity(request.availableQuantity())
+                .category(
+                        Category.builder()
+                                .id(request.categoryId())
+                                .build()
+                )
+                .build();
+    }
+
+
+    public ProductResponse toProductResponse( Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getAvailableQuantity(),
+                product.getPrice(),
+                product.getCategory().getId(),
+                product.getCategory().getName(),
+                product.getCategory().getDescription()
+        );
+    }
+
+    public ProductPurchaseResponse toProductPurchaseResponse(Product product, double quantity) {
+        return new ProductPurchaseResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                quantity
+        );
+    }
+}
